ef8b18da7d59c18226c79462e2e9e4f04cc32d3e
Brian Kim <briankim@dhcp-128-189-26-36.ubcsecure.wireless.ubc.ca>
2023-08-11 13:24:18 -0700

    Notes for __init__.py

--- rudaux/__init__.py
+++ rudaux/__init__.py
@@ -1,2 +1,5 @@
+# A regular package is typically implemented as a directory containing an __init__.py file. 
+# When a regular package is imported, this __init__.py file is implicitly executed, and the 
+# objects it defines are bound to names in the package’s namespace.
 from . import commands, flows, course_api, snapshot, autoext, grader
 from .version import __version__
