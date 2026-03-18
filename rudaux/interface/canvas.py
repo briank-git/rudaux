ef1df4bc6ce393ea365cbc6eb0de51de213bbff7
Brian Kim <brian.kim.2011@gmail.com>
2026-03-18 15:36:55 -0700

    Implement get_course_section_info

--- rudaux/interface/canvas.py
+++ rudaux/interface/canvas.py
@@ -1,8 +1,9 @@
 from loguru import logger
 
-from typing import Dict, List
+from pydantic import PrivateAttr
+from typing import Dict, List, Optional, Any
 
-from canvasapi import Canvas
+from canvasapi import Canvas as C
 
 import pendulum as plm
 from pendulum import DateTime
@@ -22,8 +23,14 @@ class Canvas(LearningManagementSystem):
     canvas_course_lms_ids: Dict[str, str]
     canvas_registration_deadlines: Dict[str, str]
     canvas_api_tokens: Dict[str, str]
-    canvas_api_instances: Dict[str, Canvas]
+    _canvas_api_instances: Any = PrivateAttr(default=None)
     assignments: Dict[str, dict]
+
+    # ---------------------------------------------------------------------------------------------------
+    def model_post_init(self, context: Any) -> None:
+        self._canvas_api_instances = {}
+        for section, id in self.canvas_course_lms_ids.items():
+            self._canvas_api_instances[section] = C(self.canvas_base_domain,self.canvas_api_tokens[section]).get_course(self.canvas_course_lms_ids[section])
 
     # ---------------------------------------------------------------------------------------------------
     def open(self):
@@ -35,11 +42,28 @@ class Canvas(LearningManagementSystem):
 
     # ---------------------------------------------------------------------------------------------------
     def get_course_section_info(self, course_section_name: str) -> CourseSectionInfo:
-        pass
+        section = self._canvas_api_instances[course_section_name]
+
+        processed_info = {
+            "lms_id": str(section.id),
+            "name": section.name,
+            "code": section.course_code,
+            "start_at": None if section.start_at is None else plm.parse(section.start_at),
+            "end_at": None if section.end_at is None else plm.parse(section.end_at),
+            "time_zone": section.time_zone
+        }
+        
+        logger.info(f"Retrieved course section info for {section.name}")
+        logger.debug(f"Processed info {processed_info}")
+        if processed_info['start_at'] is None or processed_info['end_at'] is None:
+            logger.warning(f"Course start or end date has not been set for section {section.name}")
+
+        return processed_info
 
     # ---------------------------------------------------------------------------------------------------
     def get_students(self, course_section_name) -> Dict[str, Student]:
-        pass
+        canvas = self._canvas_api_instances[course_section_name]
+        section = canvas.get_course(self.canvas_course_lms_ids[course_section_name])
 
     # ---------------------------------------------------------------------------------------------------
     def get_instructors(self, course_section_name: str) -> Dict[str, Instructor]:
