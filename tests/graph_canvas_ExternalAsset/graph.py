2129abdd6ac48a550c7438212de0e756b1ae4524
briank-git <brian.kim.2011@gmail.com>
2026-09-03 10:02:30 -0700

    Basic external asset implementation

--- tests/graph_canvas_ExternalAsset/graph.py
+++ tests/graph_canvas_ExternalAsset/graph.py
@@ -5,31 +5,55 @@ import yaml
 import fwirl
 
 from rudaux.flows import load_settings
-#from rudaux.tasks import get_grading_system
 from rudaux.model import Submission, Student, Grader, Assignment, CourseSectionInfo
-from rudaux.fwirl_components.resources import GradingSystemResource
 from rudaux.interface.base.submission_system import SubmissionGradingStatus
+from rudaux.fwirl_components.resources import LMSResource
 
 import pdb
 
 config_path='./rudaux_config.yml'
 
-with open(config_path) as f:
-    config = yaml.safe_load(f)
+config = load_settings(config_path)
 
+# Load LMS resource
+lmsresource = LMSResource(key='lmsresource',settings=config,min_query_interval=10,course_name="course_dsci_100_test")
 
 # Canvas course section info external asset
 # Gets info about the Canvas course section (id, name, code, start at, end at, timezone)
 class CanvasCourseInfoExternalAsset(fwirl.ExternalAsset):
-    pass
+    async def get(self):
+        course_section_info = lmsresource.get_course_section_info(course_section_name='section_dsci_100_test_01')
+        return course_section_info
 
-# Canvas enrollment external asset
+    async def diff(self, val):
+        if self._cached_val is None:
+            return True
+        else:
+            return val != self._cached_val
+
+# Canvas student enrollment external asset
 # Gets the class list of a Canvas course
 class CanvasEnrollmentExternalAsset(fwirl.ExternalAsset):
-    pass
+    async def get(self):
+        student_enrollment = lmsresource.get_students(course_section_name='section_dsci_100_test_01')
+        return student_enrollment
+
+    async def diff(self, val):
+        if self._cached_val is None:
+            return True
+        else:
+            return val != self._cached_val
 
 # Canvas assignments external asset
 # Gets list of Canvas assignments
 class CanvasAssignmentsExternalAsset(fwirl.ExternalAsset):
-    pass
+    async def get(self):
+        assignments = lmsresource.get_assignments(course_section_name='section_dsci_100_test_01')
+        return assignments
+
+    async def diff(self, val):
+        if self._cached_val is None:
+            return True
+        else:
+            return val != self._cached_val
 
